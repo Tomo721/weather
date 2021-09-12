515ce3e4b4f8c693e68dfe4c1ae32692401765aa
Olesya Gataulina <data27.05.12@gmail.com>
2021-09-12 09:32:37 +0300

    inline style

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const webpack = require('webpack')
 module.exports = {
+  entry: {
+    app: ['./app'],
+    styles: ['./styles']
+  },
+  loaders: [{
+    test: /\.(scss|css)$/,
+    loaders: [
+      'style',
+      'css',
+      'autoprefixer?browsers=last 3 versions',
+      'sass'
+    ]
+  }],
   configureWebpack: {
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
